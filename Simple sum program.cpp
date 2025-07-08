78bd2462b5da633ec0ccffdb084da4346091b18f
Zakria-CS <zakriauol1@gmail.com>
2025-07-08 23:52:48 +0500

    Add files via upload

---
+++ Simple sum program.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//declaration for variables.
+	int a,b,c;
+	
+	cout<<"Enter The Value of the a : ";
+	cin>>a;  //Taking input for first value
+	
+	cout<<"Enter The Value of the b : ";
+	cin>>b;  //Taking input for second value.
+	
+	c=a+b;  //First adding a and b then storing result in c by using assignment operator 
+	
+	cout<<"Sum of Two number is : "<<c<<endl; //here displaying result of sum
+	
+	return 0;
+}
