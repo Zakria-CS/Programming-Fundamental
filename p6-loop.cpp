a6b036ccbc59151a1557a64fbc824e7ed35187d8
Muhammad Zakria <zakriauol1@gmail.com>
2025-07-12 11:53:53 +0500

    Add files via upload

---
+++ p6-loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"ENTER A NUMBER : ";
+	cin>>n;
+	
+	
+	cout<<"Your program output is "<<endl;
+	
+	for(int i=0;n>=i;n--){
+		cout<<n<<endl;
+		
+	}
+	
+	
+	
+	return 0;
+}
