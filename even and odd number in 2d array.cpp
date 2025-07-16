89e272762ae9490aa2c1c904796dc995f474a327
Muhammad Zakria <zakriauol1@gmail.com>
2025-07-16 18:43:41 +0500

    Add files via upload

---
+++ even and odd number in 2d array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[3][3],even=0,odd=0;
+	cout<<"Please enter the value of the two D array.";
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cin>>arr[i][j];
+		}
+	}
+	cout<<"\nYour Entered Values are: "<<endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if(arr[i][j]%2==0){
+				even++;
+			}
+			else{
+				odd++;
+			}
+		}
+	}
+	cout<<"Total Even number in your program are : "<<even<<endl;
+	cout<<"Total odd number in your program are : "<<odd<<endl;
+	
+	
+	return 0;
+}
